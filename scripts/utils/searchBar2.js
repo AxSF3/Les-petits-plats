3cb940f01785387ae8536232b078fe8aceaf8367
AxSF3 <71151267+AxSF3@users.noreply.github.com>
2022-02-15 14:58:04 +0100

    Algorithm_2

--- scripts/utils/searchBar2.js
+++ scripts/utils/searchBar2.js
@@ -1,17 +1,47 @@
 const filteredRecipes = (recipes, searchBar) => {
 	searchBar.addEventListener("keyup", (e) => {
 		if (e.target.value.length >= 3) {
+			const results = [];
 			recipesSection.innerHTML = "";
 			const query = e.target.value;
-            function search() {
-                
+			for (let i = 0; i < recipes.length; i++) {
 
-                for (let i = 0; i < 9; i++) {
-                  recipes.ingredient = recipes.ingredient + i;
-                }
-            }    
+				const { name, ingredients, appliance, ustensils, description } = recipes[i];
+				const includesInName = name.toLowerCase().includes(query);
+				const includesInDescription = description.toLowerCase().includes(query);
+				let includesInIngredients = false;
+				let includesApparatus = false;
+				let includesUstensils = false;
+
+				for (let ii = 0; ii < ingredients.length; ii++) {
+					if (ingredients[ii].ingredient.toLowerCase().includes(query)) {
+						includesInIngredients = true;
+					}
+				}
+
+				for (let a = 0; a < appliance.length; a++) {
+					if (appliance[a].toLowerCase().includes(query)) {
+						includesApparatus = true;
+						console.log(a)
+					}
+				}
+
+				for (let u = 0; u < ustensils.length; u++) {
+					if (ustensils[u].toLowerCase().includes(query)) {
+						includesUstensils = true;
+					}
+				}
+
+				if (includesInName || includesInDescription || includesInIngredients || includesApparatus || includesUstensils) {
+					results.push(recipes[i]);
+				}
+			} 
        
-			createRecipesCard(results);
+			if (results.length) {
+				recipesSection.innerHTML = "";
+				createRecipesCard(results);
+			}
+			
 			if (!results.length) {
                 return recipesSection.innerHTML +=  `<div class="no__results"> 
                 Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc.
