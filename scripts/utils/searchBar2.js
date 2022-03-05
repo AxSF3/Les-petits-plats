cc460583cb3937f8fae75b910e1ec38f0f5c3eb2
AxSF3 <71151267+AxSF3@users.noreply.github.com>
2022-03-05 16:57:12 +0100

    Add files via upload

--- scripts/utils/searchBar2.js
+++ scripts/utils/searchBar2.js
@@ -1,58 +1,76 @@
 const filteredRecipes = (recipes, searchBar) => {
 	searchBar.addEventListener("keyup", (e) => {
 		if (e.target.value.length >= 3) {
+			
 			const results = [];
 			recipesSection.innerHTML = "";
 			const query = e.target.value;
+			//console.log(recipes)
 
 			for (let i = 0; i < recipes.length; i++) {
 
 				const { name, ingredients, appliance, ustensils, description } = recipes[i];
 				const includesInName = name.toLowerCase().includes(query);
 				const includesInDescription = description.toLowerCase().includes(query);
+
+				let find = false;
 			
 					for (let ii = 0; ii < ingredients.length; ii++) {
 						if (ingredients[ii].ingredient.toLowerCase().includes(query)) {
 							results.push(recipes[i]);
-							continue;
+							find = true;
+							break;
 						}
+					}
+
+					if (find) {
+						continue;
 					}
 			
 					for (let a = 0; a < appliance.length; a++) {
 						if (appliance[a].toLowerCase().includes(query)) {
 							results.push(recipes[i]);
-							continue;
+							find = true;
+							break;
 						}
+					}
+
+					if (find) {
+						continue;
 					}
 			
 					for (let u = 0; u < ustensils.length; u++) {
 						if (ustensils[u].toLowerCase().includes(query)) {
 							results.push(recipes[i]);
-							continue;
+							find = true;
+							break;
 						}
+					}
+
+					if (find) {
+						continue;
 					}
 
 					for (let n = 0; n < name.length; n++) {
-						if (name[n].toLowerCase().includes(query)) {
+						if (includesInName) {
 							results.push(recipes[i]);
-							continue;
+							find = true;
+							break;
 						}
 					}
 
-					let il = 0;
-					
-					while (il < 0) {
-						il++;
-  						
-						if (includesInName || includesInDescription) {
-							results.push(recipes[i]);
-    						continue;
-  						}
-
+					if (find) {
+						continue;
 					}
 
-					console.log(results)
-					
+					for (let d = 0; d < description.length; d++) {
+						if (includesInDescription) {
+							results.push(recipes[i]);
+							find = true;
+							break;
+						}
+					}
+
 			}
        
 			if (results.length) {
@@ -72,3 +90,83 @@ const filteredRecipes = (recipes, searchBar) => {
 
 	});
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const filteredRecipes = (recipes, searchBar) => {
+	searchBar.addEventListener("keyup", (e) => {
+		if (e.target.value.length >= 3) {
+
+			const { name, ingredients, appliance, ustensils, description } = recipes;
+			
+			const filter = (recipes, query) => {
+				const results = [];
+
+				let find = false;
+
+				for (const recipe of recipes) {
+					
+				}
+
+			}
+       
+			if (results.length) {
+				recipesSection.innerHTML = "";
+				createRecipesCard(results);
+			}
+			
+			if (!results.length) {
+                return recipesSection.innerHTML +=  `<div class="no__results"> 
+                Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc.
+                </div>`
+			}
+		} else if (e.target.value.length <= 3) {
+			recipesSection.innerHTML = "";
+			createRecipesCard(recipes);
+		}
+
+	});
+};*/
