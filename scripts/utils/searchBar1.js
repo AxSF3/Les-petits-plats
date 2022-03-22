f5896e489eac197d740b88b90b25646bb704b531
AxSF3 <71151267+AxSF3@users.noreply.github.com>
2022-03-23 00:17:14 +0100

    Add files via upload

--- scripts/utils/searchBar1.js
+++ scripts/utils/searchBar1.js
@@ -1,27 +1,25 @@
-const filteredRecipes = (recipes, searchBar) => {
-	searchBar.addEventListener("keyup", (e) => {
-		if (e.target.value.length >= 3) {
-			recipesSection.innerHTML = "";
-			const query = e.target.value;
-			const results = recipes.filter((recipe) => {
-				return (
-					recipe.description.includes(query) || 
-					recipe.ingredients.some((ingredient) => ingredient.ingredient.includes(query)) ||
-					recipe.appliance.toLowerCase().includes(query) ||
-					recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(query)) ||
-					recipe.name.toLowerCase().includes(query)
-				);
-			});
-			createRecipesCard(results);
-			if (!results.length) {
-                return recipesSection.innerHTML +=  `<div class="no__results"> 
-                Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc.
-                </div>`
-			}
-		} else if (e.target.value.length <= 3) {
-			recipesSection.innerHTML = "";
-			createRecipesCard(recipes);
-		}
 
-	});
-};
+
+
+
+const filteredRecipes = (recipes, query) => {
+	
+		
+			return recipes.filter((recipe) => {
+				return (
+					recipe.name.toLowerCase().includes(query) ||
+					recipe.description.includes(query) ||
+					recipe.ingredients.some((ingredient) => ingredient.ingredient.includes(query))
+				);
+				
+			});
+				
+		
+	};
+
+
+
+
+
+
+
