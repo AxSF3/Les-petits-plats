d2a45bc90a18bccdad8943d7f378de0b644478b7
AxSF3 <71151267+AxSF3@users.noreply.github.com>
2022-03-23 00:17:58 +0100

    Add files via upload

--- scripts/utils/searchBar1.js
+++ scripts/utils/searchBar1.js
@@ -1,61 +1,13 @@
-/*const filteredRecipes = (recipes, searchBar) => {
-	searchBar.addEventListener("keyup", (e) => {
-		if (e.target.value.length >= 3) {
-			recipesSection.innerHTML = "";
-			const query = e.target.value;
-			const results = recipes.filter((recipe) => {
-				return (
-					recipe.description.includes(query) || 
-					recipe.ingredients.some((ingredient) => ingredient.ingredient.includes(query)) ||
-					recipe.appliance.toLowerCase().includes(query) ||
-					recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(query)) ||
-					recipe.name.toLowerCase().includes(query)
-				);
-			});
-			createRecipesCard(results);
-			if (!results.length) {
-                return recipesSection.innerHTML +=  `<div class="no__results"> 
-                Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc.
-                </div>`
-			}
-		} else if (e.target.value.length <= 3) {
-			recipesSection.innerHTML = "";
-			createRecipesCard(recipes);
-		}
-
-	});
-};*/
-
-
-
-
-
-const filteredRecipes = (recipes) => {
+const filteredRecipes = (recipes, query) => {
 	
-		globalSearchBar.addEventListener("keyup", (e) => {
-			
-
-			const query = e.target.value;
-			const results = recipes.filter((recipe) => {
+			return recipes.filter((recipe) => {
 				return (
-					recipe.description.includes(query) || 
-					recipe.ingredients.some((ingredient) => ingredient.ingredient.includes(query)) ||
-					recipe.appliance.toLowerCase().includes(query) ||
-					recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(query)) ||
-					recipe.name.toLowerCase().includes(query)
+					recipe.name.toLowerCase().includes(query) ||
+					recipe.description.includes(query) ||
+					recipe.ingredients.some((ingredient) => ingredient.ingredient.includes(query))
 				);
 				
 			});
-			createRecipesCard(results);
-			if (!results.length) {
-				return recipesSection.innerHTML +=  `<div class="no__results"> 
-				Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc.
-				</div>`
-			}
-			
-		})
+				
+		
 	};
-
-
-
-
