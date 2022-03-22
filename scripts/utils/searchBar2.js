42944cf0ee99ff61318118a204458c21d7093a53
AxSF3 <71151267+AxSF3@users.noreply.github.com>
2022-03-23 00:22:43 +0100

    Add files via upload

--- scripts/utils/searchBar2.js
+++ scripts/utils/searchBar2.js
@@ -4,44 +4,22 @@ const filteredRecipes = (recipes, query) => {
 
 			for (let i = 0; i < recipes.length; i++) {
 
-				const { name, ingredients, description } = recipes[i];
+				const { name, description, ingredients } = recipes[i];
 
-				let find = false;
-			
-					for (let ii = 0; ii < ingredients.length; ii++) {
-						if (ingredients[ii].ingredient.toLowerCase().includes(query)) {
-							results.push(recipes[i]);
-							find = true;
-							break;
-						}
-					}
+				if (name.toLowerCase().includes(query) || description.toLowerCase().includes(query) ) {
+					results.push(recipes[i]);
+					continue;
+				}
 
-					if (find) {
-						continue;
+				for (let ii = 0; ii < ingredients.length; ii++) {
+					if (ingredients[ii].ingredient.toLowerCase().includes(query)) {
+						results.push(recipes[i]);
+						break;
 					}
-
-					for (let n = 0; n < name.length; n++) {
-						if (name.toLowerCase().includes(query)) {
-							results.push(recipes[i]);
-							find = true;
-							break;
-						}
-					}
-
-					if (find) {
-						continue;
-					}
-
-					for (let d = 0; d < description.length; d++) {
-						if (description.toLowerCase().includes(query)) {
-							results.push(recipes[i]);
-							find = true;
-							break;
-						}
-					}
+				}
 
 			}
-
+	
 			return results
 };
 
@@ -85,39 +63,3 @@ const filteredRecipes = (recipes, query) => {
 
 
 
-
-/*
-const filteredRecipes = (recipes, searchBar) => {
-	searchBar.addEventListener("keyup", (e) => {
-		if (e.target.value.length >= 3) {
-
-			const { name, ingredients, appliance, ustensils, description } = recipes;
-			
-			const filter = (recipes, query) => {
-				const results = [];
-
-				let find = false;
-
-				for (const recipe of recipes) {
-					
-				}
-
-			}
-       
-			if (results.length) {
-				recipesSection.innerHTML = "";
-				createRecipesCard(results);
-			}
-			
-			if (!results.length) {
-                return recipesSection.innerHTML +=  `<div class="no__results"> 
-                Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc.
-                </div>`
-			}
-		} else if (e.target.value.length <= 3) {
-			recipesSection.innerHTML = "";
-			createRecipesCard(recipes);
-		}
-
-	});
-};*/
